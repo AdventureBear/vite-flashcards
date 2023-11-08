3e7b24743bbc3a3e6fa7985994e506a5191d525b
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-11-08 10:51:35 -0500

    refactored button to Button

--- src/components/FeedbackModal.tsx
+++ src/components/FeedbackModal.tsx
@@ -2,6 +2,7 @@ import {useFlashCardState} from "../store.ts";
 // import {successPhrases} from "../utils/successPhrases.ts";
 import {encouragementPhrases, successPhrases} from '../utils/successPhrases.ts'
 import Modal from "../templates/Modal.tsx";
+import Button from "../templates/Button.tsx";
 
 interface FeedbackModalProps {
     close: () => void,  // close this modal
@@ -15,7 +16,7 @@ const FeedbackModal = ({ close, handleNext }: FeedbackModalProps) => {
         showFeedbackModal && (
             <>
                 <Modal >
-                    <h2 className="text-2xl mb-4 h-20 overflow-hidden">
+                    <h2 className="text-2xl  mt-2 h-20 overflow-hidden">
                         {answeredCorrectly ? (
                           successPhrases[Math.floor(Math.random() * successPhrases.length)]
                         ) : (
@@ -23,41 +24,16 @@ const FeedbackModal = ({ close, handleNext }: FeedbackModalProps) => {
                         )}
                     </h2>
 
-                    <button
-                        className="bg-green-500 text-white px-4 py-2 rounded"
+                    <Button
+                        color={2}
                         onClick={() => {
                             close();
                             handleNext();
                         }}
-                    >
-                        Next
-                    </button>
-
+                    >Next</Button>
+                    
                 </Modal>
 
-            {/*<div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">*/}
-            {/*    <div className="bg-white p-8 rounded shadow-lg text-center w-96">*/}
-            {/*        {answeredCorrectly ? (*/}
-            {/*            <h2 className="text-2xl mb-4 h-20 overflow-hidden">*/}
-            {/*                {successPhrases[Math.floor(Math.random() * successPhrases.length)]}*/}
-            {/*            </h2>*/}
-            {/*        ) : (*/}
-            {/*            <h2 className="text-2xl mb-4 h-20 overflow-hidden">*/}
-            {/*                {encouragementPhrases[Math.floor(Math.random() * encouragementPhrases.length)]}*/}
-            {/*            </h2>*/}
-            {/*        )}*/}
-
-            {/*        <button*/}
-            {/*            className="bg-green-500 text-white px-4 py-2 rounded"*/}
-            {/*            onClick={() => {*/}
-            {/*                close();*/}
-            {/*                handleNext();*/}
-            {/*            }}*/}
-            {/*        >*/}
-            {/*            Next*/}
-            {/*        </button>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
             </>
         )
     );
--- src/templates/Button.tsx
+++ src/templates/Button.tsx
@@ -3,27 +3,30 @@ import { ReactNode } from 'react';
 enum ButtonColor {
     ORANGE,
     TEAL,
+    GREEN,
     // SUCCESS,
     // INFO,
 }
 
 enum ButtonSize {
-    LARGE,
     SMALL,
+    LARGE,
+    XL,
+
 }
 
 
 
 const SIZE_MAPS: Record<ButtonSize, string> = {
     [ButtonSize.SMALL]: 'px-2.5 text-xs mb-4 mx-2',
-    [ButtonSize.LARGE]: 'text-xl py-2 px-4 mt-10 mb-12',
+    [ButtonSize.LARGE]: 'px-4 py-2 mb-4 mt-1',
+    [ButtonSize.XL]: 'py-4 px-8 mb-8 mt-4 text-xl text-bold',
 };
 
 const COLOR_MAPS: Record<ButtonColor, string> = {
     [ButtonColor.ORANGE]: 'bg-orange-300 text white',
     [ButtonColor.TEAL]: 'hover:text-white text-black font-bold  bg-teal-300 hover:bg-teal-500',
-    // [ButtonVariant.SUCCESS]: 'bg-green-100 text-green-800',
-    // [ButtonVariant.INFO]: 'bg-blue-100 text-blue-800',
+    [ButtonColor.GREEN]: 'bg-green-500 text-white font-bold ',
 };
 
 type ButtonProps = {
@@ -38,7 +41,7 @@ export default function Button(props: ButtonProps) {
     const {children, color, size, onClick} = props;
 
     const buttonLayoutClasses =
-        'inline-flex items-center  rounded shadow-lg mb-4 mx-2';
+        'inline-flex items-center rounded shadow-lg hover:scale-105';  //mb-4 mx-2
 
     const finalButtonClasses = `${buttonLayoutClasses} ${COLOR_MAPS[color]} ${SIZE_MAPS[size]}`;
 
