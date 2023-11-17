666e0ebab114bfdf4feb438fe4d48b8776b54beb
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-11-17 15:36:24 -0500

    20 changed "reviewed" to "progress" in progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -14,7 +14,7 @@ const ProgressBar = ({ deck, progressBarWidth}: ProgressBarProps) => {
         <div className="max-w-xl mx-auto mt-4 bg-gray-500 rounded-lg p-4 shadow-lg flex justify-between items-center">
             <div>
                 <p className="text-lg font-bold ml-2"><span
-                    className="text-amber-100 mr-2">Reviewed:</span>
+                    className="text-amber-100 mr-2">Progress:</span>
                 </p>
             </div>
             <div className="flex-1">
